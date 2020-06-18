80071c4adf3e91a13c5d6f1371c7a32d28736d47
Kirill Sotnikov <kisoty@gmail.com>
2020-06-19 01:31:38 +0300

    finished task

--- user/app/routes.php
+++ user/app/routes.php
@@ -23,37 +23,146 @@ return function (App $app) {
         } else {
             $user = new User();
             try {
-                $newUserId = $user->addUser($request_body['name'], $request_body['phone']);
-                $response_body = json_encode(['message' => 'User added','data' => ["Id" => $newUserId]]);
+                $newUser = $user->addUser($request_body['name'], $request_body['phone']);
+                $response_body = json_encode(['data' => $newUser]);
+                $response->getBody()->write($response_body);
+                return $response->withHeader('Content-type', 'application/json')->withStatus(201,'User created');
+            } catch (InvalidArgumentException $e) {
+                $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                return $response->withHeader('Content-type', 'application/json')->withStatus(400,'Wrong phone format');
+            } catch (ErrorException $e) {
+                $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                return $response->withHeader('Content-type', 'application/json')->withStatus(500,'Server internal error');
             } catch (Exception $e) {
                 $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                return $response->withHeader('Content-type', 'application/json');
             }
-            $response->getBody()->write($response_body);
-            return $response->withHeader('Content-type', 'application/json');
         }
     });
 
     $app->get('/user/GetUserById', function (Request $request, Response $response) {
         $request_params = $request->getQueryParams();
-        $id = $request_params['id'];
-        if (empty($id)) {
-            $response->getBody()->write('Id is required');
-            return $response->withStatus(400,'Empty param Id');
+        if (empty($request_params['id']) || !is_numeric($request_params['id'])) {
+            $response_body = json_encode(['error' => 'Parameter Id should be a number']);
+            $response->getBody()->write($response_body);
+            return $response->withStatus(400,'Id is not a number')->withHeader('Content-type', 'application/json');
         } else {
             $user = new User();
             try {
+                $id = $request_params['id'];
                 $user_data = $user->getUserById($id);
                 $response_body = json_encode(['message' => 'User found','data' => $user_data]);
+                $response->getBody()->write($response_body);
+                return $response->withHeader('Content-type','application/json');
+            } catch (ErrorException $e) {
+                $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                return $response->withStatus(500,'Server internal error')->withHeader('Content-type', 'application/json');
             } catch (Exception $e) {
                 $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                //Не знаю, с каким кодом вернуть лучше, 204 не вернет контента,
+                //а какое-то информативное сообщение хочется вернуть. Поэтому пока 200 (дефолт).
+                //Возможно, совершенно нормально передать информацию в статусе, просто в тех API,
+                //с которыми я имел дело, даже в подобных случаях возвращалось тело ответа с ошибкой.
+                return $response->withHeader('Content-type','application/json');
             }
-            $response->getBody()->write($response_body);
         }
-        return $response->withHeader('Content-type','application/json');
     });
 
-    $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
-        $group->get('/{id}', ViewUserAction::class);
+    $app->get('/user/GetUsers', function (Request $request, Response $response) {
+        $user = new User();
+        try {
+            $user_list = $user->getAllUsers();
+            $response_body = json_encode(['message' => 'Users found', 'data' => $user_list]);
+            $response->getBody()->write($response_body);
+            return $response->withHeader('Content-type', 'application/json');
+        } catch (ErrorException $e) {
+            $response_body = json_encode(['error' => $e->getMessage()]);
+            $response->getBody()->write($response_body);
+            return $response->withHeader('Content-type', 'application/json')->withStatus(500,'Server internal error');
+        }
     });
+
+    $app->delete('/user/DeleteUserById', function (Request $request, Response $response) {
+       $request_params = $request->getQueryParams();
+       $id = $request_params['id'];
+       if (empty($id) || !is_numeric($id)) {
+           $response_body = json_encode(['error' => 'Parameter Id should be a number']);
+           $response->getBody()->write($response_body);
+           return $response->withStatus(400,'Id is not a number')->withHeader('Content-type', 'application/json');
+       } else {
+           $user = new User();
+           try {
+               $user->deleteUserById($id);
+               $response->getBody()->write(json_encode(['message' => 'User deleted']));
+               return $response->withHeader('Content-Type','application/json');
+           } catch (ErrorException $e) {
+               $response_body = json_encode(['error' => $e->getMessage()]);
+               $response->getBody()->write($response_body);
+               return $response->withHeader('Content-type', 'application/json')->withStatus(500,'Server internal error');
+           } catch (Exception $e) {
+               $response_body = json_encode(['error' => $e->getMessage()]);
+               $response->getBody()->write($response_body);
+               //Аналогично с GetUserById
+               return $response->withHeader('Content-type', 'application/json');
+           }
+       }
+    });
+
+    $app->put('/user/UpdateUserById', function (Request $request, Response $response) {
+        $newUserDataRequest = $request->getParsedBody();
+        if (empty($newUserDataRequest['id']) || !is_numeric($newUserDataRequest['id'])) {
+            $response_body = json_encode(['error' => 'Parameter Id should be a number']);
+            $response->getBody()->write($response_body);
+            return $response->withStatus(400,'Id is not a number')->withHeader('Content-type', 'application/json');
+        }
+        $user = new User();
+        try {
+            $newData = $user->updateUserById($newUserDataRequest);
+            $response_body = json_encode(['data' => $newData]);
+            $response->getBody()->write($response_body);
+            return $response->withHeader('Content-type', 'application/json')->withStatus(200,'User data updated');
+        } catch (ErrorException $e) {
+            $response_body = json_encode(['error' => $e->getMessage()]);
+            $response->getBody()->write($response_body);
+            return $response->withHeader('Content-type', 'application/json')->withStatus(500,'Server internal error');
+        } catch (Exception $e) {
+            $response_body = json_encode(['error' => $e->getMessage()]);
+            $response->getBody()->write($response_body);
+            //Аналогично с GetUserById и DeleteUserById
+            return $response->withHeader('Content-type', 'application/json');
+        }
+    });
+
+    $app->get('/user/GetUserIdByPhone', function (Request $request, Response $response) {
+        $request_params = $request->getQueryParams();
+        if (empty($request_params['phone'])) {
+            $response_body = json_encode(['error' => 'Parameter phone is required']);
+            $response->getBody()->write($response_body);
+            return $response->withStatus(400,'Missing phone parameter')->withHeader('Content-type', 'application/json');
+        } else {
+            $user = new User();
+            try {
+                $phone = $request_params['phone'];
+                $user_id = $user->getUserIdByPhone($phone);
+                $response_body = json_encode(['message' => 'User found','data' => ['id' => $user_id]]);
+                $response->getBody()->write($response_body);
+                return $response->withHeader('Content-type','application/json');
+            } catch (ErrorException $e) {
+                $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                return $response->withStatus(500,'Server internal error')->withHeader('Content-type', 'application/json');
+            } catch (Exception $e) {
+                $response_body = json_encode(['error' => $e->getMessage()]);
+                $response->getBody()->write($response_body);
+                //Аналогично, что и GetUserById, DeleteUserById и UpdateUserById
+                return $response->withHeader('Content-type','application/json');
+            }
+        }
+    });
+
 };
--- user/src/Classes/User.php
+++ user/src/Classes/User.php
@@ -8,16 +8,21 @@ class User
 {
     private $json = __DIR__ . '/../../users.json';
 
-    public function addUser(string $name, string $phone) : int
+    public function addUser(string $name, string $phone) : array
     {
         if (!preg_match('/(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})/',$phone))
-            throw new \Exception('Wrong phone format');
+            throw new \InvalidArgumentException('Insert correct phone number');
 
         $start = null;
         $end = null;
         $inner_count = 0;
-        $f = fopen($this->json,'rb');
         $buff_arr = [];
+
+        try {
+            $f = fopen($this->json,'rb');
+        } catch (\Exception $e) {
+            throw new \ErrorException($e->getMessage());
+        }
 
         foreach ($this->fileReadGen($f,1) as $value) {
             if ($value === '{') {
@@ -48,21 +53,25 @@ class User
         $chunk =  fread($f,4096);
         $lastUser = json_decode(substr(strrchr($chunk,'{'),0,strlen(strrchr($chunk,'{'))-1));
         $newUserId = $lastUser->id + 1;
-        $newUser = ','.json_encode(['id' => $newUserId, 'name' => $name, 'phone' => $phone]).']';
+        $newUser = ['id' => $newUserId, 'name' => $name, 'phone' => $phone];
+        $newUserJson = ','.json_encode($newUser).']';
         fseek($f,-1,SEEK_END);
-        $write = fwrite($f,$newUser);
+        $write = fwrite($f,$newUserJson);
         fclose($f);
         if (!$write) {
-            throw new \Exception('Cannot add user');
+            throw new \ErrorException('Cannot add user');
         }
-        return $newUserId;
+        return $newUser;
     }
 
     public function getUserById (string $id) : object
     {
-        if (!is_numeric($id))
-            throw new \Exception('Id should be a number');
-        $f = fopen($this->json,'rb');
+        try {
+            $f = fopen($this->json, 'rb');
+        } catch (\Exception $e) {
+            throw new \ErrorException($e->getMessage());
+        }
+
         $buff_arr = [];
         $start = null;
         $end = null;
@@ -92,7 +101,113 @@ class User
                 $inner_count = 0;
             }
         }
-        throw new \Exception('User with given id doesn\'t exist');
+        throw new \Exception('User with given Id doesn\'t exist');
+    }
+
+    public function getAllUsers () : array
+    {
+        //К сожалению, придется считывать весь файл
+        try {
+            $content = json_decode(file_get_contents($this->json));
+            if ($content === false) {
+                throw new \ErrorException('Couldn\'t read data from file');
+            }
+        } catch (\Exception $e) {
+            throw new \ErrorException($e->getMessage());
+        }
+        return $content;
+    }
+
+    public function deleteUserById (string $id)
+    {
+        //К сожалению php не предоставляет возможности редачить файлы на ходу, придется грузить весь файл.
+        //Это явно можно сделать системными утилитами, но я, к своему величайшему стыду, пока работаю с винды,
+        //а писать что-то серверное под винду...
+        try {
+            $user_list = json_decode(file_get_contents($this->json));
+        } catch (\Exception $e) {
+            throw new \ErrorException($e->getMessage());
+        }
+
+        foreach ($user_list as $key => $user) {
+            if ($user->id == $id) {
+                unset($user_list[$key]);
+                $user_list = array_values($user_list);
+                try {
+                    file_put_contents($this->json, json_encode($user_list));
+                } catch (\Exception $e) {
+                    throw new \ErrorException($e->getMessage());
+                }
+                return true;
+            }
+        }
+        throw new \Exception('User with given Id doesn\'t exist');
+    }
+
+    public function updateUserById (array $newUserData) : array
+    {
+        //Аналогично с deleteUser
+        try {
+            $user_list = json_decode(file_get_contents($this->json));
+        } catch (\Exception $e) {
+            throw new \ErrorException($e->getMessage());
+        }
+
+        foreach ($user_list as $key => $user) {
+            if ($user->id == $newUserData['id']) {
+                $user_list[$key] = $newUserData;
+                try {
+                    file_put_contents($this->json, json_encode($user_list));
+                } catch (\Exception $e) {
+                    throw new \ErrorException($e->getMessage());
+                }
+                return $newUserData;
+            }
+        }
+        throw new \Exception('User with given Id doesn\'t exist');
+    }
+
+    public function getUserIdByPhone (string $phone) : int
+    {
+        if (!preg_match('/(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})/',$phone))
+            throw new \InvalidArgumentException('Insert correct phone number');
+
+        try {
+            $f = fopen($this->json, 'rb');
+        } catch (\Exception $e) {
+            throw new \ErrorException($e->getMessage());
+        }
+
+        $buff_arr = [];
+        $start = null;
+        $end = null;
+        $inner_count = 0;
+
+        foreach ($this->fileReadGen($f,1) as $value) {
+            if ($value === '{') {
+                if ($inner_count === 0)
+                    $start = ftell($f)-1;
+                else
+                    $inner_count++;
+            }
+            if ($value === '}') {
+                if ($inner_count === 0)
+                    $end = ftell($f);
+                else $inner_count--;
+            }
+            if ($start && $end){
+                fseek($f,$start);
+                $buff_arr= json_decode(fread($f,$end-$start));
+                if ($buff_arr->phone == $phone) {
+                    fclose($f);
+                    return $buff_arr->id;
+                }
+                $start = null;
+                $end = null;
+                $inner_count = 0;
+            }
+        }
+        throw new \Exception('User with given phone number doesn\'t exist');
     }
 
     private function fileReadGen($file_resource,int $chunk)
